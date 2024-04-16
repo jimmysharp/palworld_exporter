9d26dd0e04bd825b753eb0784f0f4112aaf94907
jimmysharp <jimmy.sharptech@gmail.com>
2024-04-17 02:35:54 +0900

    Add CLI parser

--- palworld_exporter.go
+++ palworld_exporter.go
@@ -4,16 +4,25 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/jimmysharp/palworld_exporter/collector"
 	"github.com/jimmysharp/palworld_exporter/config"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var (
+	app           = kingpin.New("palworld_exporter", "Prometheus exporter for Palworld")
+	listenAddress = app.Flag("web.listen-address", "Address to expose metrics.").Default(":18212").Envar("LISTEN_ADDRESS").String()
+	scrapeURI     = app.Flag("scrape_uri", "URI to Palworld metrics endpoint. For basic auth, use like http://admin:pass@localhost:8212/v1/api/metrics").Required().Envar("SCRAPE_URI").String()
+)
+
 func main() {
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
 	config := &config.Config{
-		ListenAddress: ":18212",
-		ScrapeURI:     "http://admin:pass@localhost:8212/v1/api/metrics",
+		ListenAddress: *listenAddress,
+		ScrapeURI:     *scrapeURI,
 	}
 	exporter := collector.NewExporter(config)
 	prometheus.MustRegister(exporter)
